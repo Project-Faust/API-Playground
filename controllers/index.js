106aacf759f8e7610eab22f804d887acc91235b2
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-05-01 21:42:15 -0400

    Fixed models index.js to tie the models together

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,8 @@
-const express = require('express');
-const router = express.Router();
-
-// const apiRoutes = require('./api');
-router.get('/', function(req, res) {
-    res.render('layouts/main');
-
-});
+const router = require('express').Router();
+const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
