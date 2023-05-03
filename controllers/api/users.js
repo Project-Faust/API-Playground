8a21b89b17afc47a7b840cf76f63c2b69c16ba22
Project Faust <pentecost.cody.lane@gmail.com>
2023-05-02 20:59:02 -0400

    unconfuse cache

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -5,18 +5,18 @@ const router = express.Router();
 const User = require('../models/user');
 
 // Hash the password before storing it in the database
-const hashPassword = async (req, res, next) => {
-  const { password } = req.body;
+// const hashPassword = async (req, res, next) => {
+//   const { password } = req.body;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    req.body.password = hashedPassword;
-    next();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+//     req.body.password = hashedPassword;
+//     next();
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
 // Compare the hashed password with the one provided during login
 const comparePassword = async (req, res, next) => {
@@ -24,20 +24,20 @@ const comparePassword = async (req, res, next) => {
 
   try {
     const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return res.status(404).json({
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(400).json({
         success: false,
-        error: 'User not found',
+        error: 'Username or password is incorrect.',
       });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({
-        success: false,
-        error: 'Invalid password entered',
-      });
-    }
+    // if (!isMatch) {
+    //   return res.status(401).json({
+    //     success: false,
+    //     error: 'Invalid password entered',
+    //   });
+    // }
 
     req.user = user;
     next();
@@ -46,7 +46,7 @@ const comparePassword = async (req, res, next) => {
   }
 };
 
-router.post('/login', hashPassword, comparePassword, (req, res) => {
+router.post('/login', (req, res) => {
   // If the user is authenticated, send a 200 status and a success message
   return res.json({
     success: true,
