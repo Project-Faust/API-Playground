c6f32d9103a262a0164ae5ff0a1ab40c9de86e67
Project Faust <pentecost.cody.lane@gmail.com>
2023-05-04 19:37:42 -0400

    updated syntax/clarity

--- controllers/api/emailLists.js
+++ controllers/api/emailLists.js
@@ -31,11 +31,11 @@ router.get('/:id', async (req, res) => {
 // add new email list
 router.post('/', async (req, res) => {
     try {
-      const emailListData = await EmailList.create(req.body);
-      res.status(200).json(emailListData);
+        const emailListData = await EmailList.create(req.body);
+        res.status(200).json(emailListData);
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  });
+});
 
 module.exports = router;
