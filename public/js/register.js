5331c70d1bff076cc568a0160d4a4413b6dff9ab
Lane Pentecost <pentecost.cody.lane@gmail.com>
2023-05-07 13:28:09 -0400

    updated url endpoints to fix accidental dead-ends

--- public/js/register.js
+++ public/js/register.js
@@ -6,14 +6,14 @@ const register = async (event) => {
     const password = await document.querySelector('#password-signup').value;
 
     if (name && email && password) {
-        const response = await fetch('/api/user/register', {
+        const response = await fetch('/api/users/register', {
             method: 'POST',
             body: JSON.stringify({ name, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
