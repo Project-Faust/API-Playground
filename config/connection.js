4a7c4d00b7437412621754890fb603d55b795eb2
Project Faust <pentecost.cody.lane@gmail.com>
2023-05-11 13:00:15 -0400

    added jawsb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+const sequelize = new Sequelize(process.env.JAWSDB_BLACK_URL,
   {
-    host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: process.env.PORT,
   }
 );
 
