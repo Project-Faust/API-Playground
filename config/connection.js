386b039f2a57aa1da8283e692ebd4f0cf03849c3
Project Faust <pentecost.cody.lane@gmail.com>
2023-05-15 22:11:13 -0400

    updated for conditional connection config

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,19 @@
-const Sequelize = require('sequelize');
+const sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.JAWSDB_BLACK_URL,
-  {
-    dialect: 'mysql',
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
